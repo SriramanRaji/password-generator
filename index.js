0aa740d7f849ba5da50409d57089183793b1f570
Sriraman Raji <sriramselvam08@gmail.com>
2022-02-07 17:27:44 +0530

    single ticks

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
-const program = require("commander");
-const chalk = require("chalk");
-const clipboardy = require("clipboardy");
+const program = require('commander');
+const chalk = require('chalk');
+const clipboardy = require('clipboardy');
 const log = console.log;
-const createPassword = require("./utils/createPassword");
-const savePassword = require("./utils/savePassword");
+const createPassword = require('./utils/createPassword');
+const savePassword = require('./utils/savePassword');
 
-program.version("1.0.0").description("A simple password generator");
+program.version('1.0.0').description('A simple password generator');
 
 program
-  .option("-l, --length <numbers>", "length of password", "8")
-  .option("-s, --save", "save password to password.txt in current directory")
-  .option("-nn, --no-numbers", "exclude numbers")
-  .option("-ns, --no-symbols", "exclude symbols and special characters")
+  .option('-l, --length <numbers>', 'length of password', '8')
+  .option('-s, --save', 'save password to password.txt in current directory')
+  .option('-nn, --no-numbers', 'exclude numbers')
+  .option('-ns, --no-symbols', 'exclude symbols and special characters')
   .parse();
 
 const { length, save, numbers, symbols } = program.opts();
@@ -29,5 +29,5 @@ if (save) {
 clipboardy.writeSync(generatedPassword);
 
 // Print Password
-log(chalk.blue("Generated Password : ") + chalk.bold(generatedPassword));
-log(chalk.yellow("Password copied to clipboard"));
+log(chalk.blue('Generated Password : ') + chalk.bold(generatedPassword));
+log(chalk.yellow('Password copied to clipboard'));
--- utils/createPassword.js
+++ utils/createPassword.js
@@ -1,20 +1,20 @@
-const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numbers = "0123456789";
-const symbols = "!@#$%^&*_-+=";
+const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*_-+=';
 
 const createPassword = (length = 8, hasNumbers = true, hasSymbols = true) => {
   let chars = alpha;
-  hasNumbers ? (chars += numbers) : "";
-  hasSymbols ? (chars += symbols) : "";
+  hasNumbers ? (chars += numbers) : '';
+  hasSymbols ? (chars += symbols) : '';
   return generatePassword(length, chars);
 };
 
 const generatePassword = (length, chars) => {
-    let password = ''
-    for(let i=0; i<length; i++) {
-        password += chars.charAt(Math.floor(Math.random() * chars.length))
-    }
-    return password
-}
+  let password = '';
+  for (let i = 0; i < length; i++) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return password;
+};
 
-module.exports = createPassword
+module.exports = createPassword;
